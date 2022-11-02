bdd297a7f28f4460705b80b22e126767ac88c9a5
Aditya443 <96006507+HakiruKamiZX@users.noreply.github.com>
2022-11-02 10:10:49 +0700

    HW2_HCI

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import HomePage from '../views/HomePage.vue'
+import PageSecond from '../views/Page2.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/Page2',
+    name: 'PageSecond',
+    component: PageSecond
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
